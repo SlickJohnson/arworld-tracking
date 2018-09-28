b4128f07b36e954d7af15008514c31a8a490d3a8
SlickJohnson <liwa.johnson@gmail.com>
2018-09-27 22:44:04 -0700

    Add face

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -25,27 +25,8 @@ class ViewController: UIViewController {
   }
 
   @IBAction func add(_ sender: Any) {
-    let doorNode = SCNNode(geometry: SCNPlane(width: 0.03, height: 0.06))
-    let node = SCNNode()
-    let boxNode = SCNNode()
-
-    doorNode.geometry?.firstMaterial?.diffuse.contents = UIColor.brown
-    doorNode.position = SCNVector3(0, -0.02, 0.053)
-
-    node.geometry = SCNPyramid(width: 0.1, height: 0.1, length: 0.1)
-    node.position = SCNVector3(0.2, 0.3, -0.2)
-    node.geometry?.firstMaterial?.specular.contents = UIColor.orange
-    node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-
-    boxNode.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
-    boxNode.position = SCNVector3(0, -0.05, 0)
-    boxNode.geometry?.firstMaterial?.specular.contents = UIColor.blue
-    boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.orange
-
-
-    sceneView.scene.rootNode.addChildNode(node)
-    node.addChildNode(boxNode)
-    boxNode.addChildNode(doorNode)
+    addHouse()
+    addFace()
   }
 
   @IBAction func reset(_ sender: Any) {
@@ -65,6 +46,52 @@ class ViewController: UIViewController {
     let rand = CGFloat(arc4random()) / CGFloat(UINT32_MAX)
     let range = abs(firstNum - secondNum) + min(firstNum, secondNum)
     return rand * range
+  }
+}
+
+extension Int {
+  var degreesToRadians: Double { return Double(self) * .pi / 180 }
+}
+
+extension Double {
+  var float: Float { return Float(self) }
+}
+
+extension ViewController {
+  func addHouse() {
+    let houseNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
+    let doorNode = SCNNode(geometry: SCNPlane(width: 0.03, height: 0.06))
+    let roofNode = SCNNode(geometry: SCNPyramid(width: 0.1, height: 0.1, length: 0.1))
+
+    houseNode.position = SCNVector3(0, -0.05, 0)
+    houseNode.geometry?.firstMaterial?.specular.contents = UIColor.blue
+    houseNode.geometry?.firstMaterial?.diffuse.contents = UIColor.orange
+
+    doorNode.position = SCNVector3(0, -0.02, 0.053)
+    doorNode.geometry?.firstMaterial?.diffuse.contents = UIColor.brown
+
+    roofNode.position = SCNVector3(0, 0.3, -0.2)
+    roofNode.geometry?.firstMaterial?.specular.contents = UIColor.orange
+    roofNode.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+
+    roofNode.addChildNode(houseNode)
+    houseNode.addChildNode(doorNode)
+    sceneView.scene.rootNode.addChildNode(roofNode)
+  }
+
+  func addFace() {
+    let cylinder = SCNNode(geometry: SCNCylinder(radius: 0.1, height: 0.15))
+    cylinder.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+    cylinder.position = SCNVector3(0, 0, -0.2)
+    cylinder.eulerAngles = SCNVector3(0, 0, 90.degreesToRadians.float)
+
+    let pyramid = SCNNode(geometry: SCNPyramid(width: 0.1, height: 0.1, length: 0.1))
+    pyramid.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+    pyramid.position = SCNVector3(0, 0, 0.05)
+    pyramid.eulerAngles = SCNVector3(90.degreesToRadians.float, 0, 0)
+
+    cylinder.addChildNode(pyramid)
+    sceneView.scene.rootNode.addChildNode(cylinder)
   }
 }
 
