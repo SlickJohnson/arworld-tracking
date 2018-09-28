b89bc03a3f26bdca824e6ae7b593797aa5f033c2
SlickJohnson <liwa.johnson@gmail.com>
2018-09-27 20:48:23 -0700

    Add house

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -17,22 +17,37 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     sceneView.session.run(configuration)
     sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+    sceneView.autoenablesDefaultLighting = true
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
-
 
   @IBAction func add(_ sender: Any) {
+    let doorNode = SCNNode(geometry: SCNPlane(width: 0.03, height: 0.06))
     let node = SCNNode()
-    node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+    let boxNode = SCNNode()
+
+    doorNode.geometry?.firstMaterial?.diffuse.contents = UIColor.brown
+    doorNode.position = SCNVector3(0, -0.02, 0.053)
+
+    node.geometry = SCNPyramid(width: 0.1, height: 0.1, length: 0.1)
+    node.position = SCNVector3(0.2, 0.3, -0.2)
+    node.geometry?.firstMaterial?.specular.contents = UIColor.orange
     node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-    node.position = SCNVector3(-0.3, -0.2, -0.5)
+
+    boxNode.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+    boxNode.position = SCNVector3(0, -0.05, 0)
+    boxNode.geometry?.firstMaterial?.specular.contents = UIColor.blue
+    boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.orange
+
+
     sceneView.scene.rootNode.addChildNode(node)
-    print("Node added")
+    node.addChildNode(boxNode)
+    boxNode.addChildNode(doorNode)
   }
+
   @IBAction func reset(_ sender: Any) {
     restartSession()
   }
@@ -42,7 +57,14 @@ class ViewController: UIViewController {
     sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
       node.removeFromParentNode()
     }
+
     sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+  }
+
+  func randomNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat {
+    let rand = CGFloat(arc4random()) / CGFloat(UINT32_MAX)
+    let range = abs(firstNum - secondNum) + min(firstNum, secondNum)
+    return rand * range
   }
 }
 
